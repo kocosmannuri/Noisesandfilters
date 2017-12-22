7e46d0e5957229c79aeafbeefda33928fe755e5a
Osman Nuri KOÇ <32075817+kocosmannuri@users.noreply.github.com>
2017-12-22 19:49:05 +0300

    Update noiseandfilter.py

--- noiseandfilter.py
+++ noiseandfilter.py
@@ -1,10 +1,13 @@
 import noise
 import numpy as np
+import noise
+import numpy as np
 import math
 import cv2
 import logging
 import os
 import sys
+from scipy import misc
 from mytools.gtsrb import GTSRB
 
 logging.basicConfig(
@@ -21,9 +24,9 @@ x = list(range(len(x_train)))
 X_train = []
 X_train = list(range(len(x_train)))
 
-parnoise = "quantization" 
+parnoise = "gauss" 
 
-def noisy(noise_type,image,std=0.9,p=0.9,q=0.4):
+def noisy(noise_type,image,std=0.3,p=0.1,q=0.0):
 
     if noise_type == "gauss":
         a = noise.GaussianNoise(std,scale = [0,255])
@@ -51,11 +54,23 @@ def psnr(imageA, imageB):
     PIXEL_MAX = 255.0
     return 20 * math.log10(PIXEL_MAX / math.sqrt(err))
 
+def lowres(image):
+    scaling_factors=(2, 3, 4)
+    width, height, dimension = image.shape
+    size = (height, width)
+    for scaling_factor in scaling_factors:
+            downscaled = misc.imresize(image, 1 / scaling_factor, 'bicubic', mode='RGB')
+            rescaled = misc.imresize(downscaled, float(scaling_factor), 'bicubic', mode='RGB')
+            low_res_image = np.clip(rescaled.astype(np.float32), 0.0, 255.0)
+            low_res_image = cv2.resize(low_res_image, size)           
+    return low_res_image
+
 
 for i in range(len(x_train)):
       X_train[i] = noisy(parnoise, x_train[i])
-      
+      X_train[i] = lowres(X_train[i])
       x[i] = psnr(X_train[i],x_train[i])
       avg = float(sum(x))/len(x)
 
-print(avg)  
+print(avg)      
+
